e49bb7483b4cf5d6daea8bf39b928065c7e21b6a
Jaxz Tan <jaxztan@Jaxzs-MacBook-Pro.local>
2025-04-19 10:54:21 +0800

    feat: update parsing file

--- src_chtan/main.cpp
+++ src_chtan/main.cpp
@@ -1,14 +1,20 @@
-#include <iostream>
-#include <sys/socket.h>
-#include <unistd.h>      // for close()
-#include <netinet/in.h>  // for sockaddr_in
-#include <arpa/inet.h>   // for inet_pton(), htons()
-#include <cstring>       // for memset()
+#include "../inc/web.hpp"
+
+// #include <iostream>
+// #include <sys/socket.h>
+// #include <unistd.h>      // for close()
+// #include <netinet/in.h>  // for sockaddr_in
+// #include <arpa·/inet.h>   // for inet_pton(), htons()
+// #include <cstring>     
 
 int main()
 {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    int fd;
+    struct http_header* header;
+    http_header* header = new http_header;
+    memset(header, 0, sizeof(http_header));
+    int sockfd;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // int fd;
     void* recv_buffer = new char[1024];
     int flag = 1;
     size_t recv_len = 1024;
@@ -16,6 +22,8 @@ int main()
         std::cerr << "Error creating socket" << std::endl;
         return 1;
     }
-    
-    recv(fd, recv_buffer, recv_len, flag);
+    recv(sockfd, recv_buffer, recv_len, flag);
+    ft_parsing(recv_buffer, recv_len, header);
+    // std::cout << "recv_buffer: " << static_cast<char*>(recv_buffer) << std::endl;
+    return 0;
 }
