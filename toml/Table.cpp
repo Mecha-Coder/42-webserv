80fb4be04656b3351231e3f657fc968c4902a534
Ryan Cheong <ryan.cheongtl@gmail.com>
2025-05-03 11:29:15 +0800

    full functionality for testing

--- toml/Table.cpp
+++ toml/Table.cpp
@@ -4,191 +4,18 @@
 
 using namespace toml;
 
-// /**
-//  * @brief constructor for the table class.
-//  * @param type The type of the table (TABLE, ARRAY, STRING, NONE).
-//  * @details Init table with the specified type.
-//  */
-// Table::Table(EToml t) : type(t), str("") {}
-
-// Table::Table(TomlMap& m) : type(TABLE), map(m) {}
-
-// Table::Table(std::string& s) : type(STRING), str(s) {}
-
-// Table::Table(Table& other) {
-// 	type = other.type;
-// 	str = other.str;
-//     vec = other.vec;
-
-// 	for (auto& pair : other.map) {
-// 		map[pair.first] = new Table(*pair.second);
-// 	}
-// 	vec = other.vec;
-// }
-
-// /**
-//  * @brief Destructor for the table class.
-//  * @details Cleans up the map by deleting all dynamically allocated tables.
-//  */
-// Table::~Table() {
-// 	FOR_EACH(TomlMap, map, it) {
-// 		if (it->second != nullptr) {
-// 			delete it->second;
-// 			it->second = nullptr;
-// 		}
-// 	}
-// }
-
-// /**
-//  * @brief Adds new table to vector, grouped in array.
-//  */
-// void Table::Push(Table& t) {
-// 	vec.push_back(t);
-// }
-
-// void Table::Insert(std::string& s, Table* t) {
-// 	if (t->isType(ARRAY)) {
-// 		if (vec.empty())
-// 			vec.push_back(Table(TABLE));
-// 		vec.back().Insert(s, t);
-// 	} else {
-// 		map[s] = t;
-// 	}
-// }
-
-// void Table::Create(std::string& s) {
-// 	Table* target = this;
-// 	if (isType(ARRAY)) {
-// 		if (vec.empty())
-// 			vec.push_back(Table(TABLE));
-// 		target = &vec.back();
-// 	}
-// 	if (!target->Get(s).isType(TABLE)) {
-// 		target->Insert(s, new Table(TABLE));
-// 	}
-// }
-
-// bool Table::isType(EToml t) {
-// 	return type == t;
-// }
-
-// void Table::SetType(EToml t) {
-// 	type = t;
-// }
-
-// void Table::SetString(std::string& s) {
-// 	str = s;
-// }
-
-// Table& Table::Last() {
-// 	if (!isType(ARRAY)) {
-// 		return Empty();
-// 	}
-// 	if (vec.empty()) {
-// 		vec.push_back(Table(TABLE));
-// 	}
-// 	return vec.back();
-// }
-
-// void Table::Print(int indent) {
-// 	std::string pad(indent, ' ');
-// 	switch (type) {
-// 	case STRING:
-// 		std::cout << pad << str << std::endl;
-// 		break;
-// 	case TABLE:
-// 		std::cout << pad << "{" << std::endl;
-// 		FOR_EACH(TomlMap, map, it) {
-// 			std::cout << pad << "  " << it->first << ": ";
-// 			it->second->Print(indent + 2);
-// 		}
-// 		std::cout << pad << "}" << std::endl;
-// 		break;
-// 	case ARRAY:
-// 		std::cout << pad << "[" << std::endl;
-// 		FOR_EACH(std::vector<Table>, vec, it) {
-// 			it->Print(indent + 2);
-// 		}
-// 		std::cout << pad << "]" << std::endl;
-// 		break;
-// 	case NONE:
-// 		std::cout << pad << "NONE" << std::endl;
-// 		break;
-// 	}
-// }
-
-// Table& Table::operator[](std::string& index) {
-// 	return Get(index);
-// }
-
-// Table& Table::operator[](std::size_t index) {
-// 	return Get(index);
-// }
-
-// toml::Table& toml::Table::operator=(Table& other) {
-// 	if (this == &other)
-// 		return *this;
-
-// 	TomlMap::iterator it = map.begin();
-// 	while (it != map.end()) {
-// 		delete it->second;
-// 		++it;
-// 	}
-// 	map.clear();
-// 	vec.clear();
-// 	str.clear();
-
-// 	this->type = other.type;
-// 	this->str = other.str;
-
-// 	FOR_EACH_CONST(Table::TomlMap, other.map, mit) {
-// 		this->map[mit->first] = new Table(*mit->second);
-// 	}
-
-// 	this->vec = other.vec;
-
-// 	return *this;
-// }
-
-// const std::string& Table::asStr(const std::string& fallback) {
-// 	return type == STRING ? str : fallback;
-// }
-
-// int Table::asInt(int fallback) {
-// 	if (type != STRING) return fallback;
-// 	char* end;
-// 	long val = std::strtol(str.c_str(), &end, 10);
-// 	return (*end == '\0') ? static_cast<int>(val) : fallback;
-// }
-
-// bool Table::asBool(bool fallback) {
-// 	if (type != STRING) return fallback;
-// 	if (str == "true" || str == "on" || str == "yes") return true;
-// 	if (str == "false" || str == "off" || str == "no") return false;
-// 	return fallback;
-// }
-
-// Table& Table::Get(std::string& s) {
-// 	if (!isType(TABLE)) return Empty();
-// 	TomlMap::iterator it = map.find(s);
-// 	return (it != map.end()) ? *(it->second) : Empty();
-// }
-
-// Table& Table::Get(std::size_t index) {
-// 	if (!isType(ARRAY) || index >= vec.size()) return Empty();
-// 	return vec[index];
-// }
-
-// Table& Table::Empty() {
-// 	static Table emptyTable(NONE);
-// 	return emptyTable;
-// }
-
+/**
+ * @brief constructor for the table class.
+ * @param type The type of the table (TABLE, ARRAY, STRING, NONE).
+ * @details Init table with the specified type.
+ */
 Table::Table(EToml t) : type(t), str("") {}
 
 Table::Table(TomlMap& m) : type(TABLE), map(m) {}
 
 Table::Table(const std::string& s) : type(STRING), str(s) {}
+
+Table::Table(bool b) : type(STRING), str(b ? "true" : "false") {}
 
 Table::Table(const Table& other) {
 	type = other.type;
@@ -200,6 +27,10 @@ Table::Table(const Table& other) {
 	}
 }
 
+/**
+ * @brief Destructor for the table class.
+ * @details Cleans up the map by deleting all dynamically allocated tables.
+ */
 Table::~Table() {
 	FOR_EACH(TomlMap, map, it) {
 		if (it->second != nullptr) {
@@ -209,6 +40,9 @@ Table::~Table() {
 	}
 }
 
+/**
+ * @brief Adds new table to vector, grouped in array.
+ */
 void Table::Push(const Table& t) {
 	vec.push_back(t);
 }
@@ -371,6 +205,53 @@ const std::vector<Table>& Table::GetVector() const {
 
 const Table::TomlMap& Table::GetMap() const {
 	return map;
+}
+
+std::string Table::GetTypeName() const {
+	switch (type) {
+		case TABLE: return "TABLE";
+		case ARRAY: return "ARRAY";
+		case STRING: return "STRING";
+		case NONE: return "NONE";
+		default: return "UNKNOWN";
+	}
+}
+
+std::vector<std::string> Table::GetKeys() const {
+	std::vector<std::string> keys;
+	if (!isType(TABLE)) return keys;
+	for (TomlMap::const_iterator it = map.begin(); it != map.end(); ++it) {
+		keys.push_back(it->first);
+	}
+	return keys;
+}
+
+bool Table::IsValid() const {
+	if (type == TABLE)
+		return !map.empty();
+	if (type == ARRAY)
+		return !vec.empty();
+	if (type == STRING)
+		return !str.empty();
+	return false;
+}
+
+std::size_t Table::Size() const {
+	if (type == TABLE)
+		return map.size();
+	if (type == ARRAY)
+		return vec.size();
+	if (type == STRING)
+		return 1;
+	return 0;
+}
+
+void Table::Remove(const std::string& key) {
+	TomlMap::iterator it = map.find(key);
+	if (it != map.end()) {
+		delete it->second;
+		map.erase(it);
+	}
 }
 
 Table& Table::Empty() {
--- toml/Table.hpp
+++ toml/Table.hpp
@@ -6,59 +6,6 @@
 #include <vector>
 #include <iostream>
 
-// namespace toml {
-
-// class Table {
-// public:
-
-// 	enum EToml {
-// 		TABLE,
-// 		ARRAY,
-// 		STRING,
-// 		NONE
-// 	};
-
-// 	typedef std::map<std::string, Table*> TomlMap;
-
-// 	explicit Table(EToml type);
-// 	Table(TomlMap& map);
-// 	explicit Table(std::string& str);
-//     Table(Table& other);
-// 	~Table();
-
-// 	Table& operator=(Table& other);
-// 	Table& operator[](std::string& key);
-// 	Table& operator[](std::size_t index);
-
-// 	void Push(Table& t);
-// 	void Insert(std::string& key, Table* value);
-// 	void Create(std::string& key);
-// 	void SetType(EToml type);
-// 	void SetString(std::string& value);
-
-// 	Table& Get(std::string& key);
-// 	Table& Get(std::size_t index);
-// 	Table& Last();
-
-// 	bool isType(EToml type);
-// 	const std::string& asStr(const std::string& fallback = "");
-// 	int asInt(int fallback = 0);
-// 	bool asBool(bool fallback = false);
-
-// 	void Print(int indent = 0);
-
-// 	static Table& Empty();
-
-// private:
-	
-// 	EToml type;
-// 	TomlMap map;
-// 	std::vector<Table> vec;
-// 	std::string str;
-// };
-
-// }
-
 namespace toml {
 
 class Table {
@@ -76,14 +23,15 @@ public:
 	explicit Table(EToml type);
 	Table(TomlMap& map);
 	explicit Table(const std::string& str);
+    explicit Table(bool b);
 	Table(const Table& other);
 	~Table();
 
 	Table& operator=(const Table& other);
 	Table& operator[](const std::string& key);
-    const Table& operator[](const std::string& key) const;
+	const Table& operator[](const std::string& key) const;
 	Table& operator[](std::size_t index);
-    const Table& operator[](std::size_t index) const;
+	const Table& operator[](std::size_t index) const;
 
 	void Push(const Table& t);
 	void Insert(const std::string& key, Table* value);
@@ -92,11 +40,11 @@ public:
 	void SetString(const std::string& value);
 
 	Table& Get(const std::string& key);
-    const Table& Get(const std::string& key) const;
+	const Table& Get(const std::string& key) const;
 	Table& Get(std::size_t index);
-    const Table& Get(std::size_t index) const;
-    const std::vector<Table>& GetVector() const;
-    const TomlMap& GetMap() const;
+	const Table& Get(std::size_t index) const;
+	const std::vector<Table>& GetVector() const;
+	const TomlMap& GetMap() const;
 	Table& Last();
 
 	bool isType(EToml type) const;
@@ -105,8 +53,13 @@ public:
 	bool asBool(bool fallback = false) const;
 
 	void Print(int indent = 0) const;
+    bool IsValid() const;
+    std::string GetTypeName() const;
+    std::vector<std::string> GetKeys() const;
+    std::size_t Size() const;
 
-	static Table& Empty();
+    void Remove(const std::string& key);
+    static Table& Empty();
 
 private:
 	EToml type;
