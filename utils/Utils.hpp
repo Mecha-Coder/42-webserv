d8e02d634d55e5ca6778d47e4c256ec1bc6484a4
Ryan Cheong <ryan.cheongtl@gmail.com>
2025-05-18 13:28:36 +0800

    new management

--- utils/Utils.hpp
+++
@@ -1,4 +0,0 @@
-#define FOR_EACH(Type, Container, It) \
-	for (Type::iterator It = (Container).begin(); It != (Container).end(); ++It)
-#define FOR_EACH_CONST(Type, Container, It) \
-	for (Type::const_iterator It = (Container).begin(); It != (Container).end(); ++It)
