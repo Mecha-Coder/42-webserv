0730b2bd25c780b6a23af375ac23c10bd8274607
Jaxz Tan <jaxztan@Jaxzs-MacBook-Pro.local>
2025-05-22 14:48:44 +0800

    update error type

--- src_chtan/header.hpp
+++ src_chtan/header.hpp
@@ -6,7 +6,7 @@
 /*   By: chtan <chtan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 12:46:44 by chtan             #+#    #+#             */
-/*   Updated: 2025/05/21 12:52:50 by chtan            ###   ########.fr       */
+/*   Updated: 2025/05/22 14:44:20 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,78 +27,82 @@
 #include <vector>
 
 typedef std::string str;
-// namespace fs = std::filesystem;
-// typedef std::filesystem fs;
-// enum nb {
-//     ERROR_505 = 505,
-//     ERROR_404 = 404
-// };
+namespace fs = std::filesystem;
+enum nb {
+    ERROR_505 = 505,
+    ERROR_404 = 404
+};
 
-// typedef enum error_code
-// {
-//     ERROR_200 = 200,
-//     ERROR_201 = 201,
-//     ERROR_202 = 202,
-//     ERROR_204 = 204,
+typedef enum error_code
+{
+    ERROR_200 = 200,
+    ERROR_201 = 201,
+    ERROR_202 = 202,
+    ERROR_204 = 204,
 
-//     ERROR_301 = 301,
-//     ERROR_302 = 302,
+    ERROR_301 = 301,
+    ERROR_302 = 302,
 
-//     ERROR_400 = 400,
-//     ERROR_401 = 401,
-//     ERROR_403 = 403,
-//     ERROR_404 = 404,
-//     ERROR_405 = 405,
-//     ERROR_408 = 408,
-//     ERROR_414 = 414,
-//     ERROR_429 = 429,
+    ERROR_400 = 400,
+    ERROR_401 = 401,
+    ERROR_403 = 403,
+    ERROR_404 = 404,
+    ERROR_405 = 405,
+    ERROR_408 = 408,
+    ERROR_414 = 414,
+    ERROR_429 = 429,
 
-//     ERROR_500 = 500,
-//     ERROR_501 = 501,
-//     ERROR_502 = 502,
-//     ERROR_503 = 503,
-//     ERROR_505 = 505,
-// };
+    ERROR_500 = 500,
+    ERROR_501 = 501,
+    ERROR_502 = 502,
+    ERROR_503 = 503,
+    ERROR_505 = 505,
+} error_code;
 
-// class header{
-//     private:
-//     const std::map<error_code, std::pair<std::string, std::string >> errorDescriptions = {
-//         {ERROR_200, {"OK", "The request was successful."}},
-//         {ERROR_201, {"Created", "The request was successful and a resource was created."}},
-//         {ERROR_202, {"Accepted", "The request has been accepted for processing, but not completed."}},
-//         {ERROR_204, {"No Content", "The server successfully processed the request, but is not returning any content."}},
-    
-//         {ERROR_301, {"Moved Permanently", "The resource has been moved to a new URL permanently."}},
-//         {ERROR_302, {"Found", "The resource resides temporarily under a different URL."}},
-    
-//         {ERROR_400, {"Bad Request", "The server could not understand the request."}},
-//         {ERROR_401, {"Unauthorized", "Authentication is required and has failed or has not been provided."}},
-//         {ERROR_403, {"Forbidden", "The server understood the request but refuses to authorize it."}},
-//         {ERROR_404, {"Not Found", "The requested resource could not be found."}},
-//         {ERROR_405, {"Method Not Allowed", "The HTTP method is not allowed for the requested resource."}},
-//         {ERROR_408, {"Request Timeout", "The server timed out waiting for the request."}},
-//         {ERROR_414, {"URI Too Long", "The URI provided was too long for the server to process."}},
-//         {ERROR_429, {"Too Many Requests", "The user has sent too many requests in a given amount of time."}},
-    
-//         {ERROR_500, {"Internal Server Error", "A generic error occurred on the server."}},
-//         {ERROR_501, {"Not Implemented", "The server does not support the functionality required."}},
-//         {ERROR_502, {"Bad Gateway", "The server received an invalid response from an upstream server."}},
-//         {ERROR_503, {"Service Unavailable", "The server is currently unavailable (overloaded or down)."}},
-//         {ERROR_505, {"HTTP Version Not Supported", "The server does not support the HTTP version used in the request."}}
-//     };
+class header {
+    private:
+        struct ErrorDescription {
+            std::string title;
+            std::string description;
+        };
 
-//     protected:
-//         error_code err;
-//         // std::string err_msg(int i);
+        static const std::pair<error_code, ErrorDescription> errorDescriptions[];
 
-//     public:
-//         header(void);
-//         header(const header &tocopy);
-//         header &operator=(const header &tocopy);
-//         ~header(void);
-//         std::string get_map(int error) const;
-        
-// };
+    protected:
+        error_code err;
+
+    public:
+        header(void);
+        header(const header &tocopy);
+        header &operator=(const header &tocopy);
+        ~header(void);
+        std::string get_map(int error) const;
+};
+
+const std::pair<error_code, header::ErrorDescription> header::errorDescriptions[] = {
+    std::make_pair(ERROR_200, ErrorDescription{"OK", "The request was successful."}),
+    std::make_pair(ERROR_201, ErrorDescription{"Created", "The request was successful and a resource was created."}),
+    std::make_pair(ERROR_202, ErrorDescription{"Accepted", "The request has been accepted for processing, but not completed."}),
+    std::make_pair(ERROR_204, ErrorDescription{"No Content", "The server successfully processed the request, but is not returning any content."}),
+
+    std::make_pair(ERROR_301, ErrorDescription{"Moved Permanently", "The resource has been moved to a new URL permanently."}),
+    std::make_pair(ERROR_302, ErrorDescription{"Found", "The resource resides temporarily under a different URL."}),
+
+    std::make_pair(ERROR_400, ErrorDescription{"Bad Request", "The server could not understand the request."}),
+    std::make_pair(ERROR_401, ErrorDescription{"Unauthorized", "Authentication is required and has failed or has not been provided."}),
+    std::make_pair(ERROR_403, ErrorDescription{"Forbidden", "The server understood the request but refuses to authorize it."}),
+    std::make_pair(ERROR_404, ErrorDescription{"Not Found", "The requested resource could not be found."}),
+    std::make_pair(ERROR_405, ErrorDescription{"Method Not Allowed", "The HTTP method is not allowed for the requested resource."}),
+    std::make_pair(ERROR_408, ErrorDescription{"Request Timeout", "The server timed out waiting for the request."}),
+    std::make_pair(ERROR_414, ErrorDescription{"URI Too Long", "The URI provided was too long for the server to process."}),
+    std::make_pair(ERROR_429, ErrorDescription{"Too Many Requests", "The user has sent too many requests in a given amount of time."}),
+
+    std::make_pair(ERROR_500, ErrorDescription{"Internal Server Error", "A generic error occurred on the server."}),
+    std::make_pair(ERROR_501, ErrorDescription{"Not Implemented", "The server does not support the functionality required."}),
+    std::make_pair(ERROR_502, ErrorDescription{"Bad Gateway", "The server received an invalid response from an upstream server."}),
+    std::make_pair(ERROR_503, ErrorDescription{"Service Unavailable", "The server is currently unavailable (overloaded or down)."}),
+    std::make_pair(ERROR_505, ErrorDescription{"HTTP Version Not Supported", "The server does not support the HTTP version used in the request."})
+};
 
 class response
 {
