c9405600e9077fb746d81845891bcbaba724eb92
Ryan Cheong <ryan.cheongtl@gmail.com>
2025-06-20 17:31:13 +0800

    toStr

--- src/toml/ParseError.hpp
+++ src/toml/ParseError.hpp
@@ -6,7 +6,7 @@
 /*   By: rcheong <rcheong@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 21:10:33 by rcheong           #+#    #+#             */
-/*   Updated: 2025/06/20 15:11:24 by rcheong          ###   ########.fr       */
+/*   Updated: 2025/06/20 17:30:48 by rcheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ public:
 	ParseError(std::string, int);
 
 	std::string asStr() {
-		return "ParseError: [" + message + "] at line " + std::to_string(line);
+		return "ParseError: [" + message + "] at line " + std::toStr(line);
 	}
 
 private:
